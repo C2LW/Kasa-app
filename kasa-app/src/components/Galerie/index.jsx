878d0b11db48ad95f1a08b6cbbb3767fa9c04a54
Lucas Wojaczek <lwojaczek@bacs-consulting.com>
2025-10-22 23:24:53 +0200

    separated cards to galerie on a component galerie

---
+++ kasa-app/src/components/Galerie/index.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+import { getData } from "../../datas/api.js"
+import Cards from "../../components/Cards/index.jsx"
+import "./styles.scss"
+
+function Galerie() {
+
+    const [announcements, setAnnouncements] = useState([])
+    /* const [error, setError] = useState(null); */
+
+    useEffect(() => {
+        getData()
+            .then((data) => {
+                setAnnouncements(data);
+            })
+            .catch((error) => {
+                console.log("Erreur de chargement des données : " + error);
+            });
+    }, []);
+
+    return (
+        <div className="galerie">
+            {announcements.length > 0 ? (
+                announcements.map(announcement => (
+                    <Cards
+                        key={announcement.id}
+                        id={announcement.id}
+                        title={announcement.title}
+                        src={announcement.cover}
+                    />
+                ))
+            ) : (
+                <p>Chargement des annonces...</p>
+            )}
+        </div>
+    )
+}
+
+export default Galerie
--- kasa-app/src/pages/Home/index.jsx
+++ kasa-app/src/pages/Home/index.jsx
@@ -1,44 +1,15 @@
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
 import Banner from "../../components/Banner"
-import { getData } from "../../datas/api.js"
-import Cards from "../../components/Cards/index.jsx"
-import { useEffect, useState } from "react"
-import "./styles.scss"
+import Galerie from "../../components/Galerie"
 
 function Home() {
 
-    const [announcements, setAnnouncements] = useState([])
-    /* const [error, setError] = useState(null); */
-
-    useEffect(() => {
-        getData()
-            .then((data) => {
-                setAnnouncements(data);
-            })
-            .catch((error) => {
-                console.log("Erreur de chargement des données : " + error);
-            });
-    }, []);
-
     return (
-        <div>
+        <div className="main">
             <Header />
             <Banner />
-            <div className="card-container">
-                {announcements.length > 0 ? (
-                    announcements.map(announcement => (
-                        <Cards
-                            key={announcement.id}
-                            id={announcement.id}
-                            title={announcement.title}
-                            src={announcement.cover}
-                        />
-                    ))
-                ) : (
-                    <p>Chargement des annonces...</p>
-                )}
-            </div>
+            <Galerie />
             <footer>
                 <Footer />
             </footer>
