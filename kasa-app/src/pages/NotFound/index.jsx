e207a6a0ff5f9a250fbba7979fe076774031c346
Lucas Wojaczek <lwojaczek@bacs-consulting.com>
2025-10-23 21:35:08 +0200

    add notFound page

--- kasa-app/src/pages/NotFound/index.jsx
+++ kasa-app/src/pages/NotFound/index.jsx
@@ -1,6 +1,25 @@
+import Header from "../../components/Header"
+import Footer from "../../components/Footer"
+import { Link } from "react-router"
+
+import "./styles.scss"
+
+
 
 function NotFound() {
-    return <h1> Erreur 404 </h1>
+    return (
+        <>
+            <div className="main">
+                <Header />
+                <div className="not-found-container">
+                    <h2>404</h2>
+                    <span>Oups! La page que vous demandez n'existe pas.</span>
+                    <Link className="navBar__link" to="/">Retourner sur la page d’accueil</Link>
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
 }
 
 export default NotFound
