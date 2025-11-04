b77b724a5f45d196555e24515cd21abe995c7cb7
Lucas Wojaczek <lwojaczek@bacs-consulting.com>
2025-11-04 12:16:14 +0100

    finalisation responsive home page

--- kasa-app/src/components/Banner/index.jsx
+++ kasa-app/src/components/Banner/index.jsx
@@ -3,26 +3,10 @@ import "./styles.scss"
 
 function Banner({ src, alt, title }) {
 
-    const trimmedTitle = title.trim();
-    const isTitle = trimmedTitle !== "";
-
-    const [firstPart = "", secondPart = ""] = trimmedTitle.split(",");
-
     return (
         <div className="banner">
             <img className="banner__img" src={src} alt={alt} />
-
-            {isTitle && (
-                <h1 className="banner__title">
-                    {firstPart}
-                    {secondPart && (
-                        <>
-                            ,<br className="mobile-break" />
-                            {secondPart}
-                        </>
-                    )}
-                </h1>
-            )}
+            <h1 className="banner__title">{title}</h1>
         </div>
     )
 }
